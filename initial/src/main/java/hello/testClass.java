922aaa37e3f3f3636b312fcafa501233519d8308
Sean Urgel <seantheurgel@gmail.com>
2018-07-07 18:23:28 +0800

    First commit

---
+++ initial/src/main/java/hello/testClass.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class testClass {
+    private int num;
+
+    testClass(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return this.num;
+    }
+}
